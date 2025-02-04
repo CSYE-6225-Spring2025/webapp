15c0865dc85bdb0aea34e2369e371df4689ebb3f
praveenkumar <vijayakumar.p@northeastern.edu>
2025-02-04 12:13:43 -0500

    added test cases

--- src/test/java/com/example/CloudDemo/CloudDemoUnitTest.java
+++ src/test/java/com/example/CloudDemo/CloudDemoUnitTest.java
@@ -1,14 +1,20 @@
 package com.example.CloudDemo;
 
 import com.example.CloudDemo.Controller.HealthCheckController;
+import com.example.CloudDemo.Model.HealthCheck;
 import com.example.CloudDemo.Repository.HealthCheckRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(HealthCheckController.class)
@@ -23,5 +29,32 @@ public class CloudDemoUnitTest {
     void testHealthCheck_Successful() throws Exception {
         mockMvc.perform(get("/healthz"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void testHealthCheck_Throw404ForWrongEndPoint() throws Exception {
+        mockMvc.perform(get("/healths"))
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    void testHealthCheck_Throw400WhenPayloadIsIncluded() throws Exception {
+        mockMvc.perform(get("/healthz").contentType(MediaType.APPLICATION_JSON).content("{\"key\":\"value\"}"))
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    void testHealthCheck_Throw405WhenHTTPMethodIsWrong() throws Exception {
+        mockMvc.perform(post("/healthz"))
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    void testHealthCheck_Throw400WhenQueryParamIncluded() throws Exception {
+        mockMvc.perform(get("/healthz").param("key","value"))
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    void testHealthCheck_Throw503WhenDatabaseActionFails() throws Exception {
+        doThrow(new RuntimeException()).when(repository).save(any(HealthCheck.class));
+        mockMvc.perform(get("/healthz"))
+                .andExpect(status().is5xxServerError());
     }
 }
